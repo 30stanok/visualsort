1ff8d66d5c5dafb8acc42634308bce7a2df6480f
Romilda Bode <romildab@cl-c6.nsk.21-school.ru>
2022-05-15 19:31:36 +0700

    table style fix

--- src/supportForGenerate/generatearray.js
+++ src/supportForGenerate/generatearray.js
@@ -1,11 +1,18 @@
+const text = document.getElementById('text');
+const btn = document.getElementById('button');
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-let rndArray = [];
-
-for(let i = 0; i < 100; i++) {
-    rndArray.push(getRandomInt(100));
+const rndArray = [];
+const generate = () => {
+    rndArray.length = 0
+    for(let i = 0; i < 100; i++) {
+        rndArray.push(getRandomInt(100));
+    }
+    text.innerHTML = rndArray;
 }
 
+btn.addEventListener('click', generate);
 console.log(rndArray);
